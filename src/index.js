53fab214bdeb95f7c19bcb5f165a6c2565f711f4
Emilia <emilia.lach.91@gmail.com>
2020-12-06 17:19:14 +0000

    subpages responsive fixed

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const hamburger = document.querySelector('.header__hamburger');
 const menu = document.getElementById("menu");
 const hero = document.getElementById ("hero");
 const navbar = document.querySelector('.header__navbar');
+const listItem = document.querySelector('.menu__point');
 
 
 
@@ -12,5 +13,12 @@ hamburger.addEventListener('click', () => {
     hero.classList.toggle("hide")
     navbar.classList.toggle("change")
 });
+
+
+// listItem.addEventListener('click', () => {
+//     menu.classList.toggle("hide")
+// });
+
+
 
 
